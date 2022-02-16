cd97b8eb0834215ddfde58edf830a2cba274c133
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-02-16 18:15:14 -0300

    feat: JS para avançar as telas 3 (Criação Quizz)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 //TESTANDO PUSH
+// Variaveis Globais
+let tela1 = document.querySelector('.tela-1');
+
+
 
 // function inicializada toda vez que clicar em Criar Quizz
 function criarQuizz() {
-    // Botão grande --> caso não tem nenhum quizz e pequeno caso já existam quizzes 
-    let botãoGrande = document.querySelector('.nenhum-quizz'); //Quando não há quizzes --> Aparece o botão grande.
-    let seusQuizzes = document.querySelector('.seus-Quizzes'); //Quando HÁ QUIZZES ISSO APARECE
+    tela1.classList.add("escondido");
+    const tela3_1 = document.querySelector('.tela-3-1');
+    tela3_1.classList.remove("escondido");
+}
 
-    // Ao clicar em "Criar Quizz" ou no "+" essa tela deve sumir, dando lugar à tela de Tela 3: Criação de Quiz
+function criarQuizzTela3_2() {
+    const tela3_1 = document.querySelector('.tela-3-1');
+    tela3_1.classList.add("escondido");
+    const tela3_2 = document.querySelector('.tela-3-2');
+    tela3_2.classList.remove("escondido");
+}
+
+function criarQuizzTela3_3() {
+    const tela3_2 = document.querySelector('.tela-3-2');
+    tela3_2.classList.add("escondido");
+    const tela3_3 = document.querySelector('.tela-3-3');
+    tela3_2.classList.remove("escondido");
+}
+
+function criarQuizzTela3_4() {
+    const tela3_3 = document.querySelector('.tela-3-3');
+    tela3_3.classList.add("escondido");
+    const tela3_4 = document.querySelector('.tela-3-4');
+    tela3_4.classList.remove("escondido");
 }
