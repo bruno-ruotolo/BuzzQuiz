50467c8646c386f20a8ce3e680d3d688685d7e49
allanornel <allanornel@gmail.com>
2022-02-16 00:32:07 -0300

    Iniciando a versao desktop, com dificuldade de colocar margin horizontal no seus-Quizzes

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@
 
 // function inicializada toda vez que clicar em Criar Quizz
 function criarQuizz() {
+    // Botão grande --> caso não tem nenhum quizz e pequeno caso já existam quizzes 
+    let botãoGrande = document.querySelector('.nenhum-quizz'); //Quando não há quizzes --> Aparece o botão grande.
+    let seusQuizzes = document.querySelector('.seus-Quizzes'); //Quando HÁ QUIZZES ISSO APARECE
 
+    // Ao clicar em "Criar Quizz" ou no "+" essa tela deve sumir, dando lugar à tela de Tela 3: Criação de Quiz
 }
