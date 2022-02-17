d712cd5f8b0c4caafc6872f93670f9b385e85554
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-02-16 22:43:26 -0300

    feat: JS da tela 3.2

--- js/script.js
+++ js/script.js
@@ -10,10 +10,11 @@ function criarQuizz() {
 }
 
 function criarQuizzTela3_1() {
-    if (criarQuizzTela3_1_Validacoes() === 1) {
+    // INVERTI PARA TESTAR A OUTRA TELA
+    if (criarQuizzTela3_1_Validacoes() === 0) {
         alert("Por favor, preencha os dados corretamente antes de prosseguir");
     }
-    else if (criarQuizzTela3_1_Validacoes() === 0) {
+    else if (criarQuizzTela3_1_Validacoes() === 1) {
         avacarTela3_2();
     }
 }
@@ -47,8 +48,10 @@ function criarQuizzTela3_1_Validacoes() {
                 erroContador = 1;
             } else if (quantidadePerguntaValue < 3) {
                 erroContador = 1;
-            } else if (quantidadeNiveisPerguntaValue < 2 || typeof quantidadeNiveisPerguntaValue !== 'number') {
+                console.log("Teste Quantidade");
+            } else if (quantidadeNiveisPerguntaValue < 2) {
                 erroContador = 1;
+                console.log("Teste Nivel");
             } else {
                 (erroContador = 0)
             };
@@ -60,11 +63,70 @@ function criarQuizzTela3_1_Validacoes() {
 }
 
 function criarQuizzTela3_2() {
+    if (criarQuizzTela3_2_Validacoes() === 1) {
+        alert("Por favor, preencha os dados corretamente antes de prosseguir");
+    }
+    else if (criarQuizzTela3_2_Validacoes() === 0) {
+        avacarTela3_3();
+    }
+}
+
+function avacarTela3_3() {
     const tela3_2 = document.querySelector('.tela-3-2');
     tela3_2.classList.add("escondido");
     const tela3_3 = document.querySelector('.tela-3-3');
     tela3_3.classList.remove("escondido");
 }
+
+function criarQuizzTela3_2_Validacoes() {
+    let erroContador = 0;
+
+    const textoPergunta = document.querySelectorAll('.texto-pergunta');
+    const corFundoPergunta = document.querySelectorAll('.cor-fundo-pergunta');
+    const textoRespostaCorreta = document.querySelector('.texto-resposta-correta');
+    const urlImagemRespostaCorreta = document.querySelector('.url-imagem-resposta-correta');
+    const textoRespostaIncorreta = document.querySelector(".texto-resposta-incorreta-1");
+
+    const arrayValidacaoCor = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    const validacoesUrlRespostaCorreta = urlImagemRespostaCorreta.value.includes('jpg') || urlImagemRespostaCorreta.value.includes('png') || urlImagemRespostaCorreta.value.includes('jpeg');
+
+    textoPergunta.forEach((textoPergunta) => {
+        if (textoPergunta.value.length < 20 || textoPergunta.value === "") {
+            erroContador = 1;
+            console.log("Teste textoPergunta");
+        }
+    });
+
+    corFundoPergunta.forEach((corFundoPergunta) => {
+        let corContador = 0;
+        for (let i = 0; i < arrayValidacaoCor.length; i++) {
+            if (!corFundoPergunta.value.includes(arrayValidacaoCor[i])) {
+                corContador++;
+                console.log(corContador);
+                if (corContador > 10) {
+                    erroContador = 1;
+                }
+            }
+        }
+        if (corFundoPergunta.value.length !== 7 || corFundoPergunta.value === "" || corFundoPergunta.value[0] !== "#") {
+            erroContador = 1;
+            console.log("Teste corFundoPergunta");
+        }
+    });
+
+    if (textoRespostaCorreta.value === "" || (!validacoesUrlRespostaCorreta)) {
+        erroContador = 1;
+        console.log("Teste textoRespostaCorreta");
+    }
+
+    if (textoRespostaIncorreta.value === "") {
+        erroContador = 1;
+    }
+
+    return erroContador;
+}
+
 
 function criarQuizzTela3_3() {
     const tela3_3 = document.querySelector('.tela-3-3');
